e1777e0c9a794956e1aa655f1bb20253cf8afbbe
x-dr <xd9722@gmail.com>
2023-05-27 16:40:12 +0800

    a

--- alist.sh
+++ alist.sh
@@ -3,29 +3,45 @@ mkdir -p /opt/alist/data/
 
 cat >/opt/alist/data/config.json <<EOF
 {
+  "force": false,
   "address": "0.0.0.0",
   "port": $PORT,
-  "assets": "$IASSETS",
+  "site_url": "",
+  "cdn": "",
+  "jwt_secret": "random generated",
+  "token_expires_in": 48,
   "database": {
-    "type": "$ADATABASE",
-    "user": "$BSQLUSER",
-    "password": "$CSQLPASSWORD",
-    "host": "$DSQLHOST",
-    "port": $ESQLPORT,
-    "name": "$FSQLNAME",
-    "table_prefix": "x_",
-    "db_file": "data.db"
+      "type": "$ADATABASE",
+      "host": "$DSQLHOST",
+      "port": $ESQLPORT,
+      "user": "$BSQLUSER",
+      "password": "$CSQLPASSWORD",
+      "name": "$FSQLNAME",
+      "db_file": "data.db",
+      "table_prefix": "x_",
+      "ssl_mode": ""
   },
   "scheme": {
     "https": false,
     "cert_file": "",
     "key_file": ""
   },
+  "temp_dir": "data/temp",
+  "bleve_dir": "data/bleve",
+  "log": {
+    "enable": true,
+    "name": "data/log/log.log",
+    "max_size": 10,
+    "max_backups": 5,
+    "max_age": 28,
+    "compress": false
+  },
+  "max_connections": 0,
+  "tls_insecure_skip_verify": true
   "cache": {
     "expiration": $GEXPIRATION,
     "cleanup_interval": $HCLEANUP_INTERVAL
-  },
-  "temp_dir": "data/temp"
+  }
 }
 EOF
 
